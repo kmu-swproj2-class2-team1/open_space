be83b974487415a5565c76b06c2f3cce24c3102c
MaengSanHa <msh0117@kookmin.ac.kr>
2017-11-14 10:12:09 +0900

    Add assignment8.py via upload

---
+++ sanha/assignment8.py
@@ -0,0 +1,120 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QLineEdit, QToolButton
+from PyQt5.QtWidgets import QSizePolicy
+from PyQt5.QtWidgets import QLayout, QGridLayout
+
+from keypad import numPadList, operatorList, constantList, functionList
+
+import calcFunctions
+
+class Button(QToolButton):
+
+    def __init__(self, text, callback):
+        super().__init__()
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        self.setText(text)
+        self.clicked.connect(callback)
+
+    def sizeHint(self):
+        size = super(Button, self).sizeHint()
+        size.setHeight(size.height() + 20)
+        size.setWidth(max(size.width(), size.height()))
+        return size
+
+
+class Calculator(QWidget):
+
+    def __init__(self, parent = None) :
+        super().__init__(parent)
+
+        # Display Window
+        self.display = QLineEdit()
+        self.display.setReadOnly(True)
+        self.display.setAlignment(Qt.AlignRight)
+        self.display.setMaxLength(15)
+
+        # Button Creation and Placement
+        numLayout = QGridLayout()
+        opLayout = QGridLayout()
+        constLayout = QGridLayout()
+        funcLayout = QGridLayout()
+
+        buttonGroups = {
+            'num': {'buttons': numPadList, 'layout': numLayout, 'columns': 3},
+            'op': {'buttons': operatorList, 'layout': opLayout, 'columns': 2},
+            'constants': {'buttons': constantList, 'layout': constLayout, 'columns': 1},
+            'functions': {'buttons': functionList, 'layout': funcLayout, 'columns': 1},
+        }
+        
+        
+
+        for label in buttonGroups.keys():
+            r = 0
+            c = 0
+            buttonPad = buttonGroups[label]
+            for btnText in buttonPad['buttons']:
+                button = Button(btnText, self.buttonClicked)
+                buttonPad['layout'].addWidget(button, r, c)
+                c += 1
+                if c >= buttonPad['columns']:
+                    c = 0
+                    r += 1
+
+        # Layout
+        mainLayout = QGridLayout()
+        mainLayout.setSizeConstraint(QLayout.SetFixedSize)
+
+        mainLayout.addWidget(self.display, 0, 0, 1, 2)
+        mainLayout.addLayout(numLayout, 1, 0)
+        mainLayout.addLayout(opLayout, 1, 1)
+        mainLayout.addLayout(constLayout, 2, 0)
+        mainLayout.addLayout(funcLayout, 2, 1)
+
+        self.setLayout(mainLayout)
+
+        self.setWindowTitle("My Calculator")
+
+
+    def buttonClicked(self):
+
+        if self.display.text() == 'Error!':
+            self.display.setText('')
+
+        button = self.sender()
+        key = button.text()
+
+        if key == '=':
+            try:
+                result = str(eval(self.display.text()))
+            except:
+                result = 'Error!'
+            finally:
+                self.display.setText(result)
+
+        elif key == 'C':
+            self.display.clear()
+
+
+        # 반복문 처리 : constantList와 functionList를 Dictionary로 바꿔서 코드 길이 감소
+
+        elif key == constantList.keys(x for x in range(4)):
+            self.display.setText(self.display.text() + constantList.values(x for x in range(4)))
+
+        elif key == functionList.keys(x for x in range(4)):              #numStr 비어있어서 아직 오류 발생
+            n = self.display.text()
+            value = functionList.values(x for x in range(4))
+            self.display.setText(str(value))
+
+        else:
+            self.display.setText(self.display.text() + key)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QApplication(sys.argv)
+    calc = Calculator()
+    calc.show()
+    sys.exit(app.exec_())
